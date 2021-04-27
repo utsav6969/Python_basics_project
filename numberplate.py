5d99c78c267902ab9709333354cc9c86b60054b4
Utsav Sharma <utsav.sharma@intelegencia.com>
2021-04-27 17:19:27 +0530

    Python file and json file

---
+++ numberplate.py
@@ -0,0 +1,28 @@
+import json
+import random
+import string
+
+jsonfile = open('numberplatedata.json', )
+jsondata = json.load(jsonfile)
+
+state = input("Enter the name of your state")
+location = input("Enter your location")
+
+
+for i in jsondata["states"]:
+    if(i["state_name"]==state):
+        statecode = i["Group_of_letters"]
+        rtocode = i["Location"][location]
+
+randomstr = []
+for i in range(0, 2):
+    randomstr.append((random.choice(string.ascii_uppercase)))
+
+randomnum = []
+for i in range(0, 4):
+        randomnum.append((random.randint(0, 9)))
+        res = "".join(map(str, randomnum))
+
+
+print(statecode + " " + rtocode + " " + ''.join(randomstr)+ " " + ''.join(map(str,randomnum)))
+jsonfile.close()
